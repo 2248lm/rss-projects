77e12c5f9f74b1df711613ffb5b967bd2fa21149
Lazzat Mukhametalinova <ux2248@gmail.com>
2021-03-22 04:22:24 +0600

    fead: realization of the keyboard function

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -3,25 +3,40 @@ document.querySelector(".fullscreen").addEventListener("click", toggleScreen);
 function toggleScreen () {
   if (!document.fullscreenElement) {document.documentElement.requestFullscreen();} else {if (document.fullscreenEnabled) {document.exitFullscreen();}}
 }
-//Piano
+//Keys
 const piano = document.querySelector(".piano");
 const keys = document.querySelectorAll(".piano-key");
+//Keybord
+const playSoundKeyboard = (event) => {
+  const key = document.querySelector(`.piano-key[data-key="${event.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  if (!key) return;
+  if (event.repeat) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("active");
+}
+const stopSoundKeyboard = (event) => {
+  const key = document.querySelector(`.piano-key[data-key="${event.keyCode}"]`);
+  if (!key) return;
+  key.classList.remove("active");
+}
 //On Click
 const playSound = (event) => {
   const key = event.target;
-  const note = document.getElementById(key.dataset.note);
+  const noteAudio = document.getElementById(key.dataset.note);
+  noteAudio.currentTime = 0;
+  noteAudio.play();
   key.classList.add("active");
-  note.currentTime = 0;
-  note.play();
   key.classList.remove("active");
 }
 //Mouse Over
 const startSound = (event) => {
   const key = event.target;
+  const noteAudio = document.getElementById(key.dataset.note);
+  noteAudio.currentTime = 0;
+  noteAudio.play();
   key.classList.add("active");
-  const note = document.getElementById(key.dataset.note);
-  note.currentTime = 0;
-  note.play();
 }
 const stopSound = (event) => {
   event.target.classList.remove("active");
@@ -29,7 +44,7 @@ const stopSound = (event) => {
 const startCorrespondOver = (event) => {
   if (event.target.classList.contains("piano-key")) {
     event.target.classList.add("active");
-  }  
+  }
   keys.forEach((elem) => {
     elem.addEventListener("click", playSound);
     elem.addEventListener("mouseover", startSound);
@@ -45,3 +60,5 @@ const stopCorrespondOver = () => {
 }
 piano.addEventListener("mousedown", startCorrespondOver, false);
 piano.addEventListener("mouseup", stopCorrespondOver);
+window.addEventListener("keydown", playSoundKeyboard);
+window.addEventListener("keyup", stopSoundKeyboard);
