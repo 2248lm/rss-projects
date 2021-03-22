dbfe1a290af4f8e06dbd8e8d3a7142d18015261a
Lazzat Mukhametalinova <ux2248@gmail.com>
2021-03-22 06:56:25 +0600

    fead: realization of the buttons function

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -3,7 +3,7 @@ document.querySelector(".fullscreen").addEventListener("click", toggleScreen);
 function toggleScreen () {
   if (!document.fullscreenElement) {document.documentElement.requestFullscreen();} else {if (document.fullscreenEnabled) {document.exitFullscreen();}}
 }
-//Keys
+//Keys and Buttons
 const piano = document.querySelector(".piano");
 const keys = document.querySelectorAll(".piano-key");
 //Keybord
@@ -62,3 +62,23 @@ piano.addEventListener("mousedown", startCorrespondOver, false);
 piano.addEventListener("mouseup", stopCorrespondOver);
 window.addEventListener("keydown", playSoundKeyboard);
 window.addEventListener("keyup", stopSoundKeyboard);
+//Notes-Letters Buttons
+const btnNotes = document.querySelector(".btn-notes");
+const btnLetters = document.querySelector(".btn-letters");
+btnNotes.classList.add("active");
+const onBtnNotes = (event) => {
+  event.target.classList.add("active");
+  keys.forEach((elem) => {
+    elem.classList.remove("piano-key-letter");
+  })
+  btnLetters.classList.remove("active");
+}
+const onBtnLetters = (event) => {
+  event.target.classList.add("active");
+  keys.forEach((elem) => {
+    elem.classList.add("piano-key-letter");
+  })
+  btnNotes.classList.remove("active");
+}
+btnNotes.addEventListener("click", onBtnNotes);
+btnLetters.addEventListener("click", onBtnLetters);
