2256d9b4072e918373167f62d7b175e5aee104f0
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-02-09 14:34:29 +0600

    feat: add songs list

---
+++ audio-player/index.js
@@ -0,0 +1,32 @@
+const songs = {
+  track1: {
+    album: "Ten Summoner's Tales (1993)",
+    singer: 'Sting',
+    song: 'Shape of My Heart',
+    genre: 'pop rock',
+  },
+  track2: {
+    album: 'So Far So Good (1993)',
+    singer: 'Bryan Adams',
+    song: 'Please Forgive Me',
+    genre: 'soft rock',
+  },
+  track3: {
+    album: 'The Marshall Mathers LP (2000)',
+    singer: 'Eminem & Dido',
+    song: 'Stan',
+    genre: 'horrorcore',
+  },
+  track4: {
+    album: 'Tragic Kingdom (1996)',
+    singer: 'No Doubt',
+    song: "Don't Speak",
+    gere: 'alternative rock',
+  },
+  track5: {
+    album: 'All the Right Reasons (2006)',
+    singer: 'Nickelback',
+    song: 'If Everyone Cared',
+    genre: 'alternative rock',
+  },
+};
